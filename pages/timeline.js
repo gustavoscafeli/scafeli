90d6a3cd66bfec2220de9e509e69f0cfd83ef0d6
Gustavo Scafeli <g.cflsouza@gmail.com>
2021-07-10 09:57:31 -0300

    fix case bug

--- pages/timeline.js
+++ pages/timeline.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import Link from "next/link";
 
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "../components/header";
+import Footer from "../components/footer";
 
 export default function Timeline() {
   return (
